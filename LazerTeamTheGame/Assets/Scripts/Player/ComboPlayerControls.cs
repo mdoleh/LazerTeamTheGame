477b2699a891a4dded255db29f5c720245fd8cf0
Mohammad Doleh <mdoleh@kent.edu>
2016-01-30 11:18:57 -0500

    Combo controls initial commit

---
+++ LazerTeamTheGame/Assets/Scripts/Player/ComboPlayerControls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Player;
+
+public class ComboPlayerControls : MonoBehaviour, IComboPlayerControls
+{
+    // idea, disable basic controls script while combo attack occuring
+    public void ShieldDash()
+    {
+        
+    }
+
+    public void DarkMatterRay()
+    {
+        
+    }
+}
---
+++ LazerTeamTheGame/Assets/Scripts/Player/IPlayerControls.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Player
+{
+    public interface IPlayerControls
+    {
+        bool MoveRight();
+        bool MoveLeft();
+        bool Jump();
+        bool Crouch();
+        bool Gun();
+        bool Shield();
+        bool Boots();
+        bool Helmet();
+    }
+}
