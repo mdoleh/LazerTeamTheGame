f6754d81dcc8634cb9b443df9b94ae2644d4dc83
Mohammad Doleh <mdoleh@kent.edu>
2019-03-20 20:07:10 -0700

    Completed basic tutorial

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: null,
+        create: null,
+        update: null
+    }
+};
